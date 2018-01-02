b19a3478a93eaf42b0ad7acdc26cc0392d5a6cc6
jihoon <jihoon7029@naver.com>
2018-01-02 23:30:57 +0900

    My_Memory_first_commit

---
+++ My_Memory/My_Memory/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  My_Memory
+//
+//  Created by Kim Jihoon on 2018. 1. 2..
+//  Copyright © 2018년 Kim_Jihoon. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ My_Memory/My_Memory/MemoData.swift
@@ -0,0 +1,17 @@
+//
+//  MemoData.swift
+//  My_Memory
+//
+//  Created by Kim Jihoon on 2018. 1. 2..
+//  Copyright © 2018년 Kim_Jihoon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MemoData{
+    var memoIdx: Int?
+    var title : String?
+    var contents :String?
+    var image : UIImage?
+    var regdate : Date?
+}
